3afa05715af292346678c8690c1a5d7f12156a60
Android <android@ort>
2022-06-02 09:14:51 -0300

    starting.....

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import Invoice from './routes/invoice';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />}></Route>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route path=':invoiceId' element={<Invoice />}></Route>
+        </Route>
+
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Routes>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
+
