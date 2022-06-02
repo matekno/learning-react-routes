a25f7d84d17d9fa2bf263fc4c191fee90a02b44c
Android <android@ort>
2022-06-02 10:28:27 -0300

    seguimos aprendiendo

--- src/routes/invoices.jsx
+++ src/routes/invoices.jsx
@@ -1,24 +1,48 @@
 import { getInvoices } from "../data";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, useSearchParams, Outlet } from "react-router-dom";
 
 
 export default function Invoices() {
     let invoices = getInvoices();
-
+    let [searchParams, setSearchParams] = useSearchParams(); //Osea, si queremos cambiar el param usamos setSearchParams.
     return (
         <div style={{ display: "flex" }}>
             <nav
                 style={{
                     borderRight: "solid 1px",
                     padding: "1rem",
-                }}>
-                {invoices.map((invoice) => (
-                    <Link
-                        style={{ display: "block", margin: "1rem 0" }}
+                }}
+            >
+                <input
+                    value={searchParams.get("filter") || ""}
+                    onChange={(event) => {
+                        let filter = event.target.value;
+                        if (filter) {
+                            setSearchParams({ filter });
+                        } else {
+                            setSearchParams({});
+                        }
+                    }}
+                />
+                {invoices.filter((invoice) => { /*Filtra por nombre en la barrita buscadora*/
+                    let filter = searchParams.get("filter");
+                    if (!filter) return true;
+                    let name = invoice.name.toLowerCase();
+                    return name.startsWith(filter.toLowerCase());
+                }).map((invoice) => (
+                    <NavLink
+                        style={({ isActive }) => { /*En lugar de un estilo estatico mandamos una funcion que recibe el param isActive. Esto lo sabemos por el router que nos lo dice. Si esta activo, se pone rojo*/
+                            return {
+                                display: "block",
+                                margin: "1rem 0",
+                                color: isActive ? "red" : "",
+                            };
+                        }}
                         to={`/invoices/${invoice.number}`}
-                        key={invoice.number}>
-                            {invoice.name}
-                    </Link>
+                        key={invoice.number}
+                    >
+                        {invoice.name}
+                    </NavLink>
                 ))}
             </nav>
             <Outlet />
